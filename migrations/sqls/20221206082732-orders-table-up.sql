a04aad83ae1c05f887107bab255a9a6b9480d9d4
SaudC21 <saudalslimany@gmail.com>
2022-12-13 00:02:33 +0300

    recreate migrations

--- migrations/sqls/20221206082732-orders-table-up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS orders (
-   id INTEGER PRIMARY KEY,
-   product_id INTEGER REFERENCES product (id),
-   quantity INTEGER,
-   user_id INTEGER REFERENCES user (id),
-   status VARCHAR(150) NOT NULL,
-);
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@ import bodyParser from 'body-parser'
 import orderRoutes from './handlers/order'
 import userRoutes from './handlers/user'
 import productRoutes from './handlers/product'
+import cors from 'cors'
 
 const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const address: string = "localhost:3000"
 
 app.use(bodyParser.json())
+
+app.use(cors())
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Storefront Home Page')
